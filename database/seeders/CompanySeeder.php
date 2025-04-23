0a6557bfd9513b6a92648233ca55dd72f00e2d02
TarasPinchuk <kkomforr@yandex.ru>
2025-04-24 02:07:22 +0300

    HOTFIX SEEDS

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -13,13 +13,13 @@ class CompanySeeder extends Seeder
         Company::create([
             'name'     => 'Благотворительный Фонд «Помощь»',
             'email'    => 'help_fund@example.com',
-            'password' => 'peresekin',
+            'password' => Hash::make('peresekin'),
         ]);
 
         Company::create([
             'name'     => 'Центр Добрых Дел',
             'email'    => 'good_center@example.com',
-            'password' => 'chooprin',
+            'password' => Hash::make('chooprin'),
         ]);
     }
 }
